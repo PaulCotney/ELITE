a0a9528a9105bea0221bff7c6978a7536b6de5f7
Anwica <anwica@cs.unc.edu>
2021-05-26 21:40:00 -0400

    Update and rename ELITE.sh to runELITE.sh

--- runELITE.sh
+++ runELITE.sh
@@ -3,12 +3,13 @@
 sample_list=./sample_data/sample_list.csv
 te_file=./sample_data/TEseq.csv
 reference=./sample_data/Reference.fa
+bowtie_dir=/pine/scr/a/n/anwica/bowtie_mouse
 species=mouse
 C=25
 T=80
 K=25
 
 python ./scripts/Step01_FindContext.py $sample_list $te_file $C $T $K
-python ./scripts/Step02_MapContext.py $sample_list $reference $species $C
+python ./scripts/Step02_MapContext.py $sample_list $reference $bowtie_dir $species $C
 python ./scripts/Step03_MergeSides.py $sample_list $te_file
 python ./scripts/Step04_FindZygosity.py $sample_list $te_file $C
