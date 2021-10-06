f2d18e9d7f1cdb5606494b3d5dabeee074fbb77e
Paul Cotney <paulcotn@email.unc.edu>
2021-10-06 13:08:34 -0400

    Updated it removing test data from code.

--- runELITE.sh
+++ runELITE.sh
@@ -4,14 +4,18 @@ module list
 
 sample_list=./sample_data/sample_list.csv
 te_file=./sample_data/TEseq_seed.csv
-reference=/pine/scr/a/n/anwica/reference/GRCm38_68.fa
+reference=./data/GRCm38_68.fa
 bowtie_dir=./bowtie
 species=mouse
 C=25
 T=80
 K=25
 
+echo "Step 01"
 python ./scripts/Step01_FindContext.py $sample_list $te_file $C $T $K
+echo "Step 02"
 python ./scripts/Step02_MapContext.py $sample_list $reference $bowtie_dir $species $C
+echo "Step 03"
 python ./scripts/Step03_MergeSides.py $sample_list $te_file
+echo "Step 04"
 python ./scripts/Step04_FindZygosity.py $sample_list $te_file $C
